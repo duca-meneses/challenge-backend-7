a10a9e17beb7b6bac8db812a68fdb30b8889641d
Carlos Eduardo <ducameneses16@gmail.com>
2023-07-26 00:26:00 -0300

    test: :white_check_mark: Adcionando testes que verificam o Status code das requisições GET, POST, PUT e DELETE no endpoint /depoimentos

---
+++ depoimentos/test/test_status_code_depoimentos.py
@@ -0,0 +1,44 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from depoimentos.models import Depoimentos
+from depoimentos.serializer import DepoimentoSerializer
+
+
+class TestStatusCode(APITestCase):
+    def setUp(self):
+        # Criando alguns objetos de teste para Depoimentos
+        self.item1 = {'nome': 'Depoimento de Teste', 'depoimento': 'Conteúdo do depoimento de teste'}
+        self.depoimento1 = Depoimentos.objects.create(nome='Depoimento 2', depoimento='Conteúdo do depoimento 2')
+        self.depoimento2 = Depoimentos.objects.create(nome='Depoimento 3', depoimento='Conteúdo do depoimento 3')
+        self.depoimento3 = Depoimentos.objects.create(nome='Depoimento 4', depoimento='Conteúdo do depoimento 4')
+        self.depoimento4 = Depoimentos.objects.create(nome='Depoimento 5', depoimento='Conteúdo do depoimento 5')
+
+    def test_status_code_Get(self):
+        """Testando a requisição Get da  view DepoimentosViewSet """
+        url = reverse('depoimentos-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_status_code_Post(self):
+        """Testando a requisição Post da  view DepoimentosViewSet"""
+        url = reverse('depoimentos-list')
+        response = self.client.post(url ,data=self.item1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_status_code_Put(self):
+        """Testando a requisição Put da view DepoimentosViewSet"""
+        url = reverse('depoimentos-detail', kwargs={'pk': self.depoimento1.pk})
+        response = self.client.put(url, data=self.item1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_status_code_Delete(self):
+        """Testando a requisição Delete da view DepoimentosViewSet"""
+        url = reverse('depoimentos-detail', kwargs={'pk': self.depoimento2.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+      
+
+
+    
--- setup/urls.py
+++ setup/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-router.register('depoimentos', DepoimentosViewSet, basename='Depoimentos')
+router.register('depoimentos', DepoimentosViewSet, basename='depoimentos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
