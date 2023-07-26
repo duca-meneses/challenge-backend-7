9866677003b22d13f10a4f1d2ee831545c22ae2d
Carlos Eduardo <ducameneses16@gmail.com>
2023-07-26 16:51:42 -0300

    feat: :sparkles: Adicionando o endpoint /destinos no projeto

--- setup/urls.py
+++ setup/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from depoimentos.views import DepoimentosViewSet, DepoimentosHomeView
+from depoimentos.views import DepoimentoViewSet, DepoimentoHomeView
+from destinos.views import DestinoViewSet
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-router.register('depoimentos', DepoimentosViewSet, basename='depoimentos')
+router.register('depoimentos', DepoimentoViewSet, basename='Depoimentos')
+router.register('destinos', DestinoViewSet, basename='Destino') 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('depoimentos-home/', DepoimentosHomeView.as_view(), name='depoimentos-home'),
+    path('depoimentos-home/', DepoimentoHomeView.as_view(), name='depoimentos-home'),
     path('', include(router.urls))
 ]
