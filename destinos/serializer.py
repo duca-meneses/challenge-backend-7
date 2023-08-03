256e1c705bdcc04a50ac6a917c49617d253053a0
Carlos Eduardo <ducameneses16@gmail.com>
2023-08-03 19:51:47 -0300

    feat: :sparkles: Fazendo a integração com a API da OpenAi

--- destinos/serializer.py
+++ destinos/serializer.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
 from destinos.models import Destino
+from destinos.validators import texto_descritivo_valido
+import openai, os
 
 class DestinoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Destino
         fields = '__all__'
+
+    def validate(self, data):
+        if texto_descritivo_valido(data['texto_descritivo']):
+
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+
+            prompt = f"Faça um resumo sobre {data['nome']} enfatizando o porque este lugar é incrível. Utilize uma linguagem informal e até 100 caracteres no máximo em cada parágrafo. Crie 2 parágrafos neste resumo"
+
+            completion = openai.Completion.create(model="text-davinci-003", prompt=prompt, max_tokens= 1024, temperature = 0.5)
+
+            response = completion.choices[0].text
+
+            data['texto_descritivo'] = response
+        return data
+
+            
---
+++ destinos/validators.py
@@ -0,0 +1,3 @@
+
+def texto_descritivo_valido(texto_descritivo):
+    return texto_descritivo == ''
