bddc053becc0dbcb6136ed536e6e44ddee911b83
Carlos Eduardo <ducameneses16@gmail.com>
2023-08-07 00:11:32 -0300

    feat: :memo: Adcionando swagger e o redoc ao Projeto

--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "depoimentos",
     "destinos",
     "corsheaders",
+    "drf_yasg2",
 ]
 
 MIDDLEWARE = [
--- setup/urls.py
+++ setup/urls.py
@@ -2,7 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from depoimentos.views import DepoimentoViewSet, DepoimentoHomeView
 from destinos.views import DestinoViewSet, DestinoHomeView
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Api do Jornada Milhas",
+        default_version='v1',
+        description="ApiRest de viagens desenvolvido por Carlos Eduardo ",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@jornadamilhas.local"),
+        license=openapi.License(name="BSD License")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 router = routers.DefaultRouter()
@@ -13,6 +30,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('depoimentos-home/', DepoimentoHomeView.as_view(), name='depoimentos-home'),
     path('destinos-home/', DestinoHomeView.as_view(), name='destinos-home' ),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
